782d8a1a853eae6fbf778f8c4dd47d5e27d7806c
Baptiste Hardelin <91209529+BaptisteHardelin@users.noreply.github.com>
2023-02-16 22:20:33 +0100

    Add react router + navbar

--- src/App.js
+++ src/App.js
@@ -1,39 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from "react-router-dom";
+import MyNavBar from "./navbar/MyNavBar";
 import AllQuotes from "./quote/AllQuotes";
 import OneQuote from "./quote/OneQuote";
-import { useState } from "react";
 
 const App = () => {
-  const [showAll, setShowAll] = useState(false);
-  const [showUnique, setUnique] = useState(false);
-
-  const handleAll = () => {
-    setShowAll(!showAll);
-  };
-
-  const handleUnique = () => {
-    setUnique(!showUnique);
-  };
-
   return (
-    <div className="App">
-      <h1>Mes Citations!</h1>
-
-      {showAll && (
-        <div>
-          <span>AllQuotes</span>
-          <AllQuotes />
-        </div>
-      )}
-      {showUnique && (
-        <div>
-          <span>OneQuote</span>
-          <OneQuote />
-        </div>
-      )}
-
-      <button onClick={() => handleAll()}>Tout</button>
-      <button onClick={() => handleUnique()}>Unique</button>
-    </div>
+    <>
+      <MyNavBar />
+      <Routes>
+        <Route exat path="/allQuotes" element={<AllQuotes />} />
+        <Route exat path="/oneQuote" element={<OneQuote />} />
+      </Routes>
+    </>
   );
 };
 
---
+++ src/navbar/MyNavBar.js
@@ -0,0 +1,19 @@
+import { Container, Navbar, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const MyNavbar = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Nav.Item>
+          <Link to="/allQuotes">Les citations</Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Link to="/oneQuote">Une citation</Link>
+        </Nav.Item>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default MyNavbar;
