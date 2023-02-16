0768624a8a62b28096f91a1af33307dffafa7cd3
Baptiste Hardelin <91209529+BaptisteHardelin@users.noreply.github.com>
2023-02-16 21:21:59 +0100

    Add a way to see or not all quote same for a single quote

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import AllQuotes from "./quote/AllQuotes";
 import OneQuote from "./quote/OneQuote";
+import { useState } from "react";
 
 const App = () => {
+  const [showAll, setShowAll] = useState(false);
+  const [showUnique, setUnique] = useState(false);
+
+  const handleAll = () => {
+    setShowAll(!showAll);
+  };
+
+  const handleUnique = () => {
+    setUnique(!showUnique);
+  };
+
   return (
     <div className="App">
       <h1>Mes Citations!</h1>
 
-      <div>
-        <span>AllQuotes</span>
-        <AllQuotes />
-      </div>
-      <div>
-        <span>OneQuote</span>
-        <OneQuote />
-      </div>
+      {showAll && (
+        <div>
+          <span>AllQuotes</span>
+          <AllQuotes />
+        </div>
+      )}
+      {showUnique && (
+        <div>
+          <span>OneQuote</span>
+          <OneQuote />
+        </div>
+      )}
+
+      <button onClick={() => handleAll()}>Tout</button>
+      <button onClick={() => handleUnique()}>Unique</button>
     </div>
   );
 };
