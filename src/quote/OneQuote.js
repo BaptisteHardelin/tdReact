165fd3824ee7665d53e13ba6e139d28058a42eb4
Baptiste Hardelin <91209529+BaptisteHardelin@users.noreply.github.com>
2023-02-16 21:15:23 +0100

    Add a way to see the next and the previous quote with button actions

--- src/quote/OneQuote.js
+++ src/quote/OneQuote.js
@@ -6,9 +6,30 @@ const OneQuote = () => {
   const [currIndex, setCurrIndex] = useState(0);
   const currentQuote = quotes[currIndex];
 
+  const handleNext = () => {
+    // On passe à la citation suivante
+    setCurrIndex(currIndex + 1);
+
+    // Si currIndex est équivalant au nombre de citation alors on revien au début du tableau
+    if (currIndex === quotes.length - 1) {
+      setCurrIndex(0);
+    }
+  };
+
+  const handlePrev = () => {
+    if (currIndex === 0) {
+      setCurrIndex(quotes.length - 1);
+    } else {
+      setCurrIndex(currIndex - 1);
+    }
+  };
+
   return (
     <div className="App">
       {<Quote text={currentQuote.text} author={currentQuote.author} />}
+
+      <button onClick={() => handleNext()}>Suivant</button>
+      <button onClick={() => handlePrev()}>Précédent</button>
     </div>
   );
 };
